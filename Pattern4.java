d00216e4f3151b430c9a30bbf68e4f285d1c1c4c
Tanish <94254649+tanish-1@users.noreply.github.com>
2022-09-25 19:21:46 +0530

    Update Pattern4.java

--- Pattern4.java
+++ Pattern4.java
@@ -3,13 +3,13 @@ public class Main
 {
 	public static void main(String[] args) {
 	    int n = 4;
-	     int c = 1;
+	    
 	   // int t = 11;
 	    for(int i=1;i<=n;i++){
 	           for(int j = 1; j<=n-i;j++){
 	               System.out.print(" ");
 	           }
-	           
+	            int c = 1;
 	       for(int j = 1; j<=i;j++){
 	        
 	              System.out.print(c+" ");
